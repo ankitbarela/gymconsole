2ac0f8b49916bcdd0aeb2b59bf8b8d15ad657459
ankitbarela <86785066+ankitbarela@users.noreply.github.com>
2021-07-02 04:52:28 -0700

    Add files via upload

---
+++ gym tracker/gym tracker/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gym_tracker
+{
+   public enum Commands
+   {
+        CreateUser,
+        User,
+        WorkOut,
+        CreateWorkOut,
+        MyWorkOut,
+        CreateMyWorkOut,
+    }
+}
---
+++ gym tracker/gym tracker/GetString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gym_tracker
+{
+    class GetString
+    {
+        public static string GetConnectionString()
+        {
+            return "Data Source = .; Initial Catalog =GymFamily ;Integrated Security=true;";
+        }
+    }
+}
